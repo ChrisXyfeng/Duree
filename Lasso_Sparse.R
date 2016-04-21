dc9f9b359924ac84b5d91f36dcd7aa12ad44908f
ChrisXyfeng <xyfengchris@gmail.com>
2016-04-21 16:51:52 +0200

    Update and rename Lasso.R to Lasso_Sparse.R

--- Lasso_Sparse.R
+++ Lasso_Sparse.R
@@ -16,10 +16,6 @@ label_train <- train$duree
 train$duree <- train$Selected <- train$moisent <- train$moissor <- train$entree <- train$sortie <- train$ID <- NULL
 
 train <- cbind(age=train$age, as.data.frame(lapply(train[,-1], as.factor)))
-#Interaction Diag+Service
-train$ds <- paste0(as.character(train$diag), as.character(train$service))
-train$ds <- as.factor(train$ds)
-str(train)
 
 matrix_train <- sparse.model.matrix(~age+sexe+diag+service, data = train)
 #Model Lasso in package Glmnet
